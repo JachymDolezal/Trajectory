75eaf2b0a7e2e6dff2d116298fd69720deff0d16
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2020-10-24 11:30:17 +0200

    new version of Trajectory

--- Trajectories.py
+++ Trajectories.py
@@ -5,58 +5,52 @@ import numpy as np
 class Ballsim:
 
     def __init__(self):
-        self.angle = 30 # in degrees
-        self.velocity = 40 # no unit
+        self.angle = 3 # in degrees
+        self.velocity = 30 # no unit
         self.Vx = 0 # horizontal vector
         self.Vy = 0 # vertical vector
         self.radian = 0
-        self.list_x = []
-        self.list_y = []
-        self.edge = [200,300,5]
-        self.a = 1/2*9.81 #constant
-        self.t = 0 #variable that changes
+        self.list_x = [] #list where y coordinates are being saved
+        self.list_y = [] #list where x coordinates are being saved
+        self.edge = [200,300,3] #dimentions of a box
+        self.a = 1/2*9.81 #gravitaitonal constant
+        self.t = 0 #variable that changes representing time
         self.h = 0 #inital height
-        self.x = 0
-        self.y = 0
-        self.time_step = 0.01
+        self.x = 0 #represents x coordinate
+        self.y = 0 #represents x coordinate
+        self.time_step = 0.01 #freqeuency of change of t variable
         self.d = 10 # intial x axis
-        self.p = 1
-        self.gradient = 0
-
-
+        self.Vy_temp = []
+ # calculates vectors from velocity and angle
     def Vectors(self):
         self.Vx = self.velocity*math.cos(self.radian)
         self.Vy = self.velocity*math.sin(self.radian)
         #print("Vx = ",self.Vx)
-        #print("Vy = ",self.Vy)
-
+        print("Vy = ",self.Vy,"ddddddddd")
+# converts degrees to radians
     def rad(self):
         pi = math.pi
         self.radian = (self.angle*pi/180)
         #print("radian = ",self.radian)
 
-    def grad(self):
-        self.gradient = (self.list_y[-1]-self.list_y[-2])/(self.list_x[-1]-self.list_x[-2])
-        #print(self.gradient)
-
+# calculates x coordinate for particular t
     def launch_x(self):
         self.x = self.Vx*self.t + self.d
         print("x = ",self.x)
         #print("d =",self.d)
-
+# calculates y coordinate for particular t
     def launch_y(self):
         self.y = self.h+self.Vy*self.t-self.a*self.t*self.t
         print("y = ",self.y)
-
+# main function that is running the program
     def trajectory(self):
         self.rad()
         self.Vectors()
         ground_hit = False
         box_hit = False
-
-        while self.Vy > 0.01:
-        #for x in range(1,30):
-            while ground_hit == False or box_hit == False:
+# program stops when when the bounce is too small (Vy > 0.01)
+        while self.Vy > 0.05:
+            while ground_hit == False:
 
                 print(self.Vy)
                 self.launch_x()
@@ -67,40 +61,48 @@ class Ballsim:
             #        self.y = 0
             #        self.t = 0
             #        print('hi')
+            #bounce of the ground
                 if self.y < 0:
                     self.d = self.x
                     self.y = 0
                     self.t = 0
+                    if box_hit == True:
+                        self.Vy = self.Vy_temp[0]
                     self.Vy = self.Vy*0.8
-                    print('//////////////////////////////////')
+                    box_hit = False
                     break
-                    """
-                if (self.x > self.edge[0] and self.x < self.edge[1]) and self.y < self.edge[2]:
+            #bouce of the top of the box
+                elif (self.x > self.edge[0] and self.x < self.edge[1]) and self.y < self.edge[2]:
+                    box_hit = True
+                    self.Vy_temp.append(self.Vy)
+                    self.Vy = self.y/self.t
+                   # print(self.h)
                     self.d = self.x
                     self.t = 0
-                    self.h = 5.1
-                    print('hey ')
-                    self.Vy = (self.list_y[-1]-self.list_y[-2])*100
-                    print(self.Vy)
+                    self.h = self.edge[2]+0.01
+                    self.y = 0
+                   # print(self.t)
+                  #  print(self.Vy)
                     #self.Vy = self.Vy*0.3
                     #print(self.Vy)
                     break
-                    """
+
                 self.list_x.append(self.x)
                 self.list_y.append(self.y)
                 self.t = self.t + self.time_step
         self.graph()
-
+# not implemented yet
     def ground_bounce(self):
         pass
-
+# not implemented yet
     def box_bounce(self):
         pass
 
-
+# graphs the lists of coordinates
     def graph(self):
         plt.plot(self.list_x,self.list_y)
         plt.show()
 
+# calls the class
 p = Ballsim()
 p.trajectory()
